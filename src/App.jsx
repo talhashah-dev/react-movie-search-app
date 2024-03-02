2db80129d1a1703caa67c52d8a0256603ea40e71
Syed Talha <syedt.in00@gmail.com>
2024-03-02 21:47:15 +0500

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from 'react'
 
-import SearchParams from "./pages/SearchParams";
-import Details from "./pages/Details";
 function App() {
-  const queryClient = new QueryClient();
-
   return (
     <div>
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          <h1>Adopt Me!</h1>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </QueryClientProvider>
-      </BrowserRouter>
+      <h1>Hello!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
