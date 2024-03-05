ab3d9f3daa61ff8941d9331bfa58ec572e0bfed2
Syed Talha <syedt.in00@gmail.com>
2024-03-05 17:42:10 +0500

    modefied project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import Search from "./components/Search";
-import MoviesContainer from "./components/MoviesContainer";
 
 function App() {
   return (
-    <div className="wrapper">
-      <Search />
-      <MoviesContainer />
-    </div>
+    <h1>Hello!</h1>
   );
 }
 
